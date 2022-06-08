8301958b0d4b9c03ec0980615473404c3754dbbc
Wang Yaofu <voipman@sina.cn>
2022-06-08 17:27:13 +0800

    Update and rename sha1.go to hash.go

--- hash/hash.go
+++ hash/hash.go
@@ -1,6 +1,7 @@
 package hash
 
 import (
+	"crypto/md5"
 	"crypto/sha1"
 	"encoding/hex"
 )
@@ -11,3 +12,11 @@ func Sha1(s string) string {
 	o.Write([]byte(s))
 	return hex.EncodeToString(o.Sum(nil))
 }
+
+// Md5 hash
+func Md5(s string) string {
+	o := md5.New()
+	o.Write([]byte(s))
+	return hex.EncodeToString(o.Sum(nil))
+}
+
