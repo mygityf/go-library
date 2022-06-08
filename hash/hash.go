51fab682b8c5f25a6914277750697bdccf726d53
Wang Yaofu <voipman@sina.cn>
2022-06-08 17:29:41 +0800

    add sha

--- hash/hash.go
+++ hash/hash.go
@@ -1,14 +1,38 @@
 package hash
 
+
 import (
 	"crypto/md5"
 	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
 	"encoding/hex"
 )
 
 // Sha1 sha1 hash impl
 func Sha1(s string) string {
 	o := sha1.New()
+	o.Write([]byte(s))
+	return hex.EncodeToString(o.Sum(nil))
+}
+
+// Sha224
+func Sha224(s string) string {
+	o := sha256.New224()
+	o.Write([]byte(s))
+	return hex.EncodeToString(o.Sum(nil))
+}
+
+// Sha256
+func Sha256(s string) string {
+	o := sha256.New()
+	o.Write([]byte(s))
+	return hex.EncodeToString(o.Sum(nil))
+}
+
+// Sha512
+func Sha512(s string) string {
+	o := sha512.New()
 	o.Write([]byte(s))
 	return hex.EncodeToString(o.Sum(nil))
 }
